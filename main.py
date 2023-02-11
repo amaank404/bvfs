cad63a46faa071281d014058d0e56c0f82c665fd
xcodz-dot <71920621+amaank404@users.noreply.github.com>
2023-02-11 23:00:03 +0530

    Added more comprehensive example

--- main.py
+++ main.py
@@ -4,13 +4,27 @@ pybvfs.core.createFs("fs.bvfs")
 fs = pybvfs.core.BVFS("fs.bvfs")
 fs.mkdir("/abc")
 fs.mkdir("/abc/xyz")
-for x in range(20):
+for x in range(15):
     fs.mkdir(f"/dirno{x}")
 
 print(fs.lsdir("/"))
 print(fs.lsdir("/abc"))
 print(fs.lsdir("/abc/xyz"))
 
+for x in range(10):
+    fs.rmdir(f"/dirno{x}")
+
+print(pybvfs.fsdump.dumpsystem(fs._fp))
+
+fs.rmdir("/dirno10")
+fs.mkdir("/abcdef")
+fs.mkdir("/abcdef/abc231")
+for x in range(15):
+    fs.mkdir(f"/dirno{x}")
+print(fs.lsdir("/"))
+
 fs.close()
 
-#print(pybvfs.fsdump.dumpsystem("fs.bvfs"))
+with open("fs.bvfs", "r+b") as fp:
+    pybvfs.fsfix.removeTruncatingBlocks(fp)
+    print(pybvfs.fsdump.dumpsystem(fp))
